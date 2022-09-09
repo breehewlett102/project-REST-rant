318aec980ecc48c8d94104d4a34cce000c249505
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-08 21:02:57 -0700

    Adding places controller

---
+++ controllers/places.js
@@ -0,0 +1,9 @@
+const res = require('express/lib/response')
+
+const router =  require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+module.exports = router
