e1766dffac08b233c9e9ae59c983bd98dcba6a26
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-15 17:58:20 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,28 @@
 const res = require('express/lib/response')
-
 const router =  require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'https://as1.ftcdn.net/v2/jpg/00/94/61/92/1000_F_94619271_LwlfXwWpAW4Hknuba5OABpJ9voXVBIFN.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'https://as1.ftcdn.net/v2/jpg/02/50/35/12/1000_F_250351248_CX51XrhDNJREgyrX8wKwdpXiu9YOhJmk.jpg'
-      }]      
-    res.render('places/new', {places})
-    
+    res.render('places/index', {places})
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
 
 module.exports = router
